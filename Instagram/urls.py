8ece62aa85594e5af7609124df7738b5b93bcd19
mzazakeith <mzazakeith@gmail.com>
2018-07-26 22:06:53 +0300

    authentication urls accounts, backends , logout added

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -1,2 +1,6 @@
 from django.conf.urls import url,include
+from django.contrib.auth import views
     url(r'^',include('mzazagram.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page":'/accounts/login'}),
+]
