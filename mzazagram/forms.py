53571ea9245cf775a0dd4e580771b835e69b29f9
mzazakeith <mzazakeith@gmail.com>
2018-07-26 22:08:20 +0300

    created a signup form

---
+++ mzazagram/forms.py
@@ -0,0 +1,8 @@
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
