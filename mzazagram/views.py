0a2ca848aa1d4f081b5a75c70a5104f64c91dd3d
mzazakeith <mzazakeith@gmail.com>
2018-07-27 14:42:50 +0300

    creating a profile view done (on registration )

--- mzazagram/views.py
+++ mzazagram/views.py
@@ -4,3 +4,19 @@
 def explore(request):
     images = Image.objects.all().order_by('-pub_date')
     return render(request, 'explore.html', {"images": images})
+
+@login_required
+def create_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = CreateProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+            return redirect('/profile')
+    else:
+        form = CreateProfileForm()
+    return render(request, 'registration/activated.html', {"form": form})
+
+
