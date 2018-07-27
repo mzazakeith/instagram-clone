ec8d96937c1ac0111bc0364e9961bbf42d528a9a
mzazakeith <mzazakeith@gmail.com>
2018-07-27 14:46:06 +0300

    searching for users profile

--- mzazagram/views.py
+++ mzazagram/views.py
@@ -92,6 +92,18 @@ def create_profile(request):
     return render(request, 'registration/activated.html', {"form": form})
 
 
+def search_results(request):
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profile = Profile.search_by_username(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"profile": searched_profile})
+
+    else:
+        message = "You haven't searched for any user"
+        return render(request, 'search.html',{"message":message})
+
 @login_required
 def comment(request,image_id):
    if request.method == 'POST':
