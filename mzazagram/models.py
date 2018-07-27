a2c1a2cd42aaba7e31bc96e474abb47e902d5bff
mzazakeith <mzazakeith@gmail.com>
2018-07-27 14:28:25 +0300

    image method with its class methods , save update caption made

--- mzazagram/models.py
+++ mzazagram/models.py
@@ -11,3 +11,21 @@ class Profile(models.Model):
     def __str__(self):
         return self.bio
 
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_caption = models.CharField(max_length=300)
+    owner = models.ForeignKey(User)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image_caption
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_caption(cls, self=image_caption):
+        self.update()
